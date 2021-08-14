b2d4c0ab4ebac87efe3fd6f8a55ac7cc950ee945
NicolasV4 <88794755+NicolasV4@users.noreply.github.com>
2021-08-14 15:35:52 -0300

    Se genera arrayNumeros
    Se obtiene el valor de array de numeros para ser utilizado en cada operación

--- tarea-clase-5.4.js
+++ tarea-clase-5.4.js
@@ -6,32 +6,36 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
-let $numeros = document.querySelectorAll('li')
+const $numeros = document.querySelectorAll('li')
+let arrayNumeros = [];
+
+for ( i = 0; i < $numeros.length ; i++){
+    arrayNumeros.push(Number($numeros[i].innerText))
+}
+
+function dividir(numero1, numero2){
+    return numero1 / numero2
+}
 
 //calcular promedios
 let sumatoriaDeNumeros = 0; //calcula total de los numeros
 
-for(let i = 0; i < $numeros.length; i++){                
-    sumatoriaDeNumeros += Number($numeros[i].innerText);
+for(let i = 0; i < arrayNumeros.length; i++){                
+    sumatoriaDeNumeros += arrayNumeros[i];
 }
 
-function promedio(numero1,cantidad){
-    return numero1 / cantidad
-}
-
-document.querySelector('#promedio').innerText = promedio(sumatoriaDeNumeros,$numeros.length)
-
+document.querySelector('#promedio').innerText = dividir(sumatoriaDeNumeros,arrayNumeros.length)
 
 
 //calcular numero menor
 let valorNumeroMenor;
 
-for (let i = 0; i < $numeros.length; i++){
-    let menor = Number($numeros[i].innerText)
+for (let i = 0; i < arrayNumeros.length; i++){
+    let menor = arrayNumeros[i];
     if(valorNumeroMenor<menor){
         menor = valorNumeroMenor
     }else {
-        valorNumeroMenor = Number($numeros[i].innerText)
+        valorNumeroMenor = arrayNumeros[i];
     }
 }
 
@@ -40,12 +44,12 @@ document.querySelector('#pequeño').innerText = valorNumeroMenor
 //calcular numero mayor
 let valorNumeroMayor;
 
-for (let i = 0; i < $numeros.length; i++){
-    let mayor = Number($numeros[i].innerText)
+for (let i = 0; i < arrayNumeros.length; i++){
+    let mayor = arrayNumeros[i];
     if(valorNumeroMayor>mayor){
         mayor = valorNumeroMayor
     }else {
-        valorNumeroMayor = Number($numeros[i].innerText)
+        valorNumeroMayor = arrayNumeros[i];
     }
 }
 
@@ -54,18 +58,18 @@ document.querySelector('#grande').innerText = valorNumeroMayor
 //calcular el numero mas repetido
 
 let repetidorMayor = 0;
-for (let i = 0; i < $numeros.length; i++){
+for (let i = 0; i < arrayNumeros.length; i++){
     let repetidor = 0;
 
 
-    for (let j = 0; j < $numeros.length; j++){
-        if ($numeros[i].innerText === $numeros[j].innerText){
+    for (let j = 0; j < arrayNumeros.length; j++){
+        if (arrayNumeros[i] === arrayNumeros[j]){
             repetidor +=1;
         }
     }
     
     if (repetidor > repetidorMayor){
         repetidorMayor = repetidor
-        document.querySelector('#comun').innerText = $numeros[i].innerText
+        document.querySelector('#comun').innerText = arrayNumeros[i];
     }
 }
